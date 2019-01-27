95c308b060c1f31f123dc6f3e4ab9ec3937653e7
Junpei Tochikawa <j.tochikawa@JunpeinoMacBook-puro.local>
2019-01-27 16:57:50 +0900

    商品のカテゴリーと名前を送るphp

---
+++ php/showProductData.php
@@ -0,0 +1,31 @@
+<?php
+try {
+
+	// connect
+	$db = new PDO("sqlite:\maruoka\maruoka_db");
+
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+	$sql = "PRAGMA foreign_keys = ON";
+	$db->query($sql);
+
+	$sql = "select category_id, name, price from products";
+	$res = $db -> query($sql);
+	$data = $res -> fetchAll();
+
+	// cutting
+	$db = null;
+
+	$response["product_data"] = $data;
+
+	$product_data = json_encode($response);
+
+} catch (Exception $e) {
+
+	echo $e->getMessage() . PHP_EOL;
+  $product_data = null;
+
+}
+?>
